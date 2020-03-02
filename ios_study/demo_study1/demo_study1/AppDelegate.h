9429c6596ffd491f816af225029bc8d984197772
jiayuqiang <1475232148@qq.com>
2020-03-02 23:53:30 +0800

    1

---
+++ ios_study/demo_study1/demo_study1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  demo_study1
+//
+//  Created by jiayuqiang on 2020/3/2.
+//  Copyright © 2020 money. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ios_study/demo_study1/demo_study1Tests/demo_study1Tests.m
@@ -0,0 +1,37 @@
+//
+//  demo_study1Tests.m
+//  demo_study1Tests
+//
+//  Created by jiayuqiang on 2020/3/2.
+//  Copyright © 2020 money. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface demo_study1Tests : XCTestCase
+
+@end
+
+@implementation demo_study1Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
